a22283b6834b2e14033d566904810c0e00b38434
Jorge Carlos Urteaga Reyesvera <jurteagar@gmail.com>
2022-01-30 21:22:18 -0600

    fix name and adding last solution

--- array/rotate_left.py
+++ array/rotate_left.py
@@ -1,23 +1,26 @@
-def rotate_left(ar, d: int):
-	"""
-	ar array with elements
-	d : point to rotate
-	1 2 3 4 5 6
-	result 
-	5 6 1 2 3 4
-	"""
-	temp = []
-	temp1 = []
-	n = len (ar)
-	for i in range (0, n):
-		if  i <n-d: # 6 -2 = 4
-			temp.append(ar[i])
-		else:
-			temp1.append(ar[i])
-	for i in range (0, len(temp)):
-		temp1.append(temp[i])
+def rotate_left(ar, d:int):
+    """
+    ar is an int's array
+    d is the index were we want to rotate
+    example:
+    ar: 1 2 3 4 5 6
+    d : 2
+    result 
+    3 4 5 6 1 2 
+    """
+    temp = []
+    # main logic
+    for i in range(0, len(ar)):
+        # keeping the numbers/elements to moved
+        if i < d: 
+            temp.append(ar[i])
+        # swaping
+        else:
+            ar[i-d] = ar[i]
+    # adding first elements.     
+    # ar + temp
+    for i in range (0, d):
+        ar.append(temp[i])
+    return ar
 
-	return temp1
-
-
-
+    
